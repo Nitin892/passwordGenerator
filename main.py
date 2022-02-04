77c5136ca375f9f4147a60fac4d2852b0fd656cc
Nitin892 <nitinshenigaram7860@gmail.com>
2022-02-04 14:14:57 +0530

    second commit

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ def passwordgen(length):
     a=list(string.ascii_lowercase)
     a.extend(list(string.ascii_uppercase))
     a.extend(list(string.punctuation))
+    a.extend(list(string.digits))
+
     password=""
     for _ in range(length):
         password+=random.choice(a)
     return password
 
-print(passwordgen(15))
+print(passwordgen(10))
